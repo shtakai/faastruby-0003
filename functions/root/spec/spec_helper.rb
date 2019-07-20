b7cf4fc043e6ed4b6684805fc7eaac1e07d4a596
Alyson Takai <shtakai@gmail.com>
2019-07-20 15:06:04 +0900

    Add setting up VCR

--- functions/root/spec/spec_helper.rb
+++ functions/root/spec/spec_helper.rb
@@ -1,3 +1,11 @@
 require 'faastruby-rpc/test_helper'
 require 'faastruby/spec_helper'
 include FaaStRuby::SpecHelper
+
+require 'pry'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.hook_into :webmock
+end
