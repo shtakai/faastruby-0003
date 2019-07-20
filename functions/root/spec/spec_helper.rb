8578d9b1d0c8da36107cd527574ed23abdc7a609
Alyson Takai <shtakai@gmail.com>
2019-07-20 15:08:20 +0900

    Change cassette directory

--- functions/root/spec/spec_helper.rb
+++ functions/root/spec/spec_helper.rb
@@ -2,10 +2,13 @@ require 'faastruby-rpc/test_helper'
 require 'faastruby/spec_helper'
 include FaaStRuby::SpecHelper
 
+require "json_matchers/rspec"
 require 'pry'
 require 'vcr'
 
+JsonMatchers.schema_root = "spec/support/api/schemas"
+
 VCR.configure do |config|
-  config.cassette_library_dir = "fixtures/vcr_cassettes"
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
 end
